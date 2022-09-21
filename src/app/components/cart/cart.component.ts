0630eacf2193163df69a1536a40e8b3447eff425
mohamed abo raia <mohamedzaghlol74111@gmail.com>
2022-09-21 23:23:31 +0200

    add cart component

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/Item';
+import { ProductsService } from 'src/app/Services/products.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  items: Item[] = [];
+  total: number = 0;
+  fullName: string = "";
+  address: string = "";
+  cardNumber: number = 0;
+  constructor(private productService : ProductsService) { }
 
   ngOnInit(): void {
+    this.GetItems();
   }
 
+  onSubmit() : void {
+    alert("sfgs");
+  }
+
+  private GetItems() {
+    this.productService.GetProducts().subscribe(res => {
+      for (let i = 0; i < res.length; i++) {
+        var item = res[i];
+        item['count'] = 1;
+        this.total += item.price * item.count;
+      }
+      this.items = res;
+    });
+  }
 }
